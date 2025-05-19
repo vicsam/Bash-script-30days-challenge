b1f599e07a833f90f0bf04013528c6db50502890
vicsam <vicsamoade@gmail.com>
2025-05-20 00:26:56 +0100

    day1

---
+++ week1/day1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Day 1: Introduction to Bash Script
+
+# Show the present working directory
+echo "This is your current working directory"
+pwd
+
+# Create folder structure from week 1 to 4
+mkdir -p week{1..4}
+
+echo "Folders are been created"
+ 
+# print the created folders
+ ls
